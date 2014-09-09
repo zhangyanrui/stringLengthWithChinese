acda4b8d20cc0d8d3cd7dc5f5dd9a4428c49e645
zhangyanrui <zhangyanrui@live.com>
2014-09-09 18:04:36 +0800

    OK

--- BoolChinese/ViewController.m
+++ BoolChinese/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *lengthLabel;
 
 @end
 
@@ -18,12 +20,41 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textFieldTextDidChange) name:UITextFieldTextDidChangeNotification object:nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Observer Method
+- (void)textFieldTextDidChange
+{
+    NSString *textStr = self.textField.text;
+    NSInteger length = [self lengthOfStringWithChinese:textStr];
+    self.lengthLabel.text = [NSString stringWithFormat:@"字符长度:%d",length];
+}
+
+
+#pragma mark - Tools
+
+
+- (NSInteger)lengthOfStringWithChinese:(NSString *)aStr
+{
+    NSInteger length = 0;
+    for(int i = 0; i < [aStr length]; i++) {
+        int a = [aStr characterAtIndex:i];
+        if( a > 0x4e00 && a < 0x9fff){//是汉字
+            length += 2;
+        }else{//普通字符
+            length += 1;
+        }
+    }
+    
+    return length;
 }
 
 @end
